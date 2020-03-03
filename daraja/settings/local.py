59cad044d0524b2403c7ae920132c5e009e5c5cc
Asinine Fatuity <asininefatuity@gmail.com>
2020-03-03 04:23:52 +0300

    added pyscopg2 to local database config

--- daraja/settings/local.py
+++ daraja/settings/local.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'daraja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'uonsda_test',
         'USER': 'postgres',
         'PASSWORD': 'asinine2020',
