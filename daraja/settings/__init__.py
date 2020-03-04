817c16378e17c1e28aeb8a86668527fe3c51c004
Asinine Fatuity <asininefatuity@gmail.com>
2020-03-04 14:34:04 +0300

    changed prod.py from urlconfig

--- daraja/settings/__init__.py
+++ daraja/settings/__init__.py
@@ -1,8 +1,8 @@
 from .production import * # when in production mode
 
-#try:
-#  from .local import *
-#except expression as identifier:
-#   pass
+try:
+  from .local import *
+except expression as identifier:
+   pass
 
 #remember to push to heroku only the production part , the local will bring errors
